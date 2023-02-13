095df9cfe0f67edf541dfba66fba643d4c0a2b4d
Pen <vusiphu20042000@gmail.com>
2023-02-13 21:50:37 +0700

    phu

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage";
 import Login from "./pages/login/index.js";
+import Register from "./pages/register/Register";
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route index element={<HomePage />} />
-                    <Route path="/home" element={<HomePage />} />
+                <Route path="/home" element={<HomePage />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </BrowserRouter>
     );
