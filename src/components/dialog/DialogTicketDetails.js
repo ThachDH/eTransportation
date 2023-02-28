77a9adffbd27f731aff10adf160f67e00804950a
Thach <hoducthach123@gmail.com>
2023-02-28 19:02:20 +0700

    ADMIN commit

--- src/components/dialog/DialogTicketDetails.js
+++ src/components/dialog/DialogTicketDetails.js
@@ -1,84 +1,130 @@
 import * as React from "react";
 import {
-    Button,
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogActions,
-    TextField,
-    Stack,
-    Divider,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TablePagination,
-    TableRow,
-    Radio,
-    Card,
-    CardContent,
-    FormControl,
-    Box,
-    Tabs,
-    Tab
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+  Stack,
+  Divider,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Radio,
+  Card,
+  CardContent,
+  FormControl,
+  Box,
+  Tabs,
+  Tab,
+  Typography
 } from "@mui/material";
 import MuiAlert from '@mui/material/Alert';
-
-import Snackbar from '@mui/material/Snackbar';
-import FlagIcon from '@mui/icons-material/Flag';
-import CenteredTabs from "./CenteredTabs";
+import PropTypes from 'prop-types';
 
 const Alert = React.forwardRef(function Alert(props, ref) {
-    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
 class DialogTicketDetails extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isOpen: false,
-            // --------- alert state -------
-            alert: {
-                isOpen: false,
-                message: 'Lỗi không xác định!',
-                duration: 5000,
-                type: 'info' // info / warning / error / success
-            },
-        }
-
-    }
-    closeDialog(dataSend) {
-        this.props.handleCloseDialog(dataSend);
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 0,
+      isOpen: false,
+      // --------- alert state -------
+      alert: {
+        isOpen: false,
+        message: 'Lỗi không xác định!',
+        duration: 5000,
+        type: 'info' // info / warning / error / success
+      },
     }
 
-    render() {
+  }
+  closeDialog(dataSend) {
+    this.props.handleCloseDialog(dataSend);
+  }
+  tabSelectHandler(e, value) {
+    this.setState({ selectedTab: value })
+  }
+  render() {
+    return (
+      <>
+        <Dialog alog open={this.props.dialog.isOpen} scroll="paper">
+          <DialogTitle variant="h5">Thông tin chi tiết</DialogTitle>
+          <Divider />
+          <DialogContent>
+            <Card sx={{ width: '500px', height: '400px' }}>
+              <CardContent>
+                <Box sx={{ width: '100%' }}>
+                  <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <Tabs
+                value={this.state.selectedTab}
+                onChange={this.tabSelectHandler.bind(this)}
+                indicatorColor="secondary"
+                textColor="inherit"
+                // variant="fullWidth"
+                aria-label="full width tabs example"
+              >
+                      <Tab label="Hình ảnh"  />
+                      <Tab label="Tiện ích"  />
+                      <Tab label="Điểm đón, trả"  />
+                    </Tabs>
+                  </Box>
+                  <TabPanel value={this.state.selectedTab} index={0} >
+                    x
+                  </TabPanel>
+                  <TabPanel value={this.state.selectedTab} index={1}>
+                    text
+                  </TabPanel>
+                  <TabPanel value={this.state.selectedTab} index={2}>
+                    text
+                  </TabPanel>
+                </Box>
+              </CardContent>
+            </Card>
+          </DialogContent>
+          <Button
+            onClick={() => this.closeDialog()}
+            variant="contained">
+            Đóng
+          </Button>
+        </Dialog>
+      </>
 
-        return (
-            <>
-                <Dialog alog open={this.props.dialog.isOpen} scroll="paper">
-                    <DialogTitle variant="h5">Thông tin chi tiết</DialogTitle>
-                    <Divider />
-                    <DialogContent>
-                        <Card sx={{ width: '500px', height: '400px' }}>
-                            {/* <Tabs value={value} onChange={handleChange} centered>
-                                <Tab label="Item One" />
-                                <Tab label="Item Two" />
-                                <Tab label="Item Three" />
-                            </Tabs> */}
-                            <CenteredTabs />
-                        </Card>
-                    </DialogContent>
-                    <Button
-                        onClick={() => this.closeDialog()}
-                        variant="contained">
-                        Đóng
-                    </Button>
-                </Dialog>
-            </>
-
-        );
-    }
+    );
+  }
 }
 export default DialogTicketDetails;
 
---
+++ src/pages/company/index.js
@@ -0,0 +1,101 @@
+import { Box, Container, Grid } from '@mui/material';
+import {Budget}  from '../../components/dashboard/budget'
+import {LatestOrders} from '../../components/dashboard/latest-orders'
+import {Sales} from '../../components/dashboard/sales'
+import { TasksProgress } from '../../components/dashboard/tasks-progress'
+import { TotalCustomers } from '../../components/dashboard/total-customers'
+import { TotalProfit } from '../../components/dashboard/total-profit'
+import {DashboardLayout} from '../../components/dashboard/dashboard-layout'
+import Footer from '../../components/footer/Footer';
+
+const Page = () => (
+  <>
+  <h2 style={{ textAlign: 'center', color: 'red' }}>Chào mừng bạn đến với công ty 3 thành viên</h2>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth={false}>
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+          >
+            <Budget />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TotalCustomers />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TasksProgress />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TotalProfit sx={{ height: '100%' }} />
+          </Grid>
+          <Grid
+            item
+            lg={8}
+            md={12}
+            xl={9}
+            xs={12}
+          >
+            <Sales />
+          </Grid>
+          <Grid
+            item
+            lg={4}
+            md={6}
+            xl={3}
+            xs={12}
+          >
+          </Grid>
+          {/* <Grid
+            item
+            lg={8}
+            md={12}
+            xl={9}
+            xs={12}
+          >
+            <LatestOrders />
+          </Grid> */}
+        </Grid>
+      </Container>
+    </Box>
+    <Footer/>
+  </>
+);
+
+Page.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
+
+export default Page;
